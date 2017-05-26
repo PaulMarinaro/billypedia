a2a0ca3d9f004ffe284e9c13fa2c5b77016fb57c
Olivia Sabo-Rush <liv@Olivias-MacBook.local>
2017-05-26 13:27:18 -0500

    update package.json to only have necessary dependencies

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,45 +1,33 @@
 var _ = require('underscore');
-var path = require('path');
-var fs = require('fs');
 var Browser = require('zombie');
 var browser = new Browser();
 var data = require('../data.json');
 
-// Path to HTML file
-const HTMLdocument = path.join(__dirname, '..', './.master/index.html');
-const readFile = fs.readFileSync(HTMLdocument);
-// Path to HTML file
 const server = 'http://localhost:3000/';
 
-describe('Billypedia Zombie', function(){
+describe('Billypedia', function(){
 
   beforeEach(function(done) {
     browser.visit(server, done);
   });
 
-  xit('should have a title Billypedia', function(done){
+  it('should have a title Billypedia', function(done){
     // console.log(browser.text('title'));
     browser.assert.text('title', 'Billypedia');
     done();
   });
 
-  // it('should have a jQuery CDN', function () {
-  //   const src = 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js';
-  //   browser.assert.attribute('script', 'src', src);
-  //   done();
-  // });
-
-  xit('should have one image of billy', function(done){
+  it('should have one image of billy', function(done){
     browser.assert.elements('#image-billy', 1);
     done();
   });
 
-  xit('should have three images on page', function(done){
+  it('should have three images on page', function(done){
     browser.assert.elements('img', 3);
     done();
   });
 
-  xit('should change image of Billy when clicked', function(done){
+  it('should change image of Billy when clicked', function(done){
     data.images.billy.forEach(function(src) {
       browser.assert.attribute('#image-billy', 'src', src);
       browser.click('#image-billy');
@@ -48,31 +36,40 @@ describe('Billypedia Zombie', function(){
     done();
   });
 
-  xit('should have a table for all rider data', function(done){
-    browser.assert.elements('table', 1);
-    browser.assert.elements('tr', 14);
-    browser.assert.elements('td', 28);
-    done();
-  });
-
-	xit('should have five top rated titles', function(done){
-		browser.assert.elements('.top-rated-title', 5);
+	it('should have five top rated titles', function(done){
+		browser.assert.elements('.top-rated-title', 5, 'Make sure your list items have the class "top-rated-title"');
 		done();
 	});
 
   it('should change top rated album image when clicking an album name', function(done){
     const src = num => data.discography.topRated[num].art;
-    browser.assert.attribute('#image-container-top-rated > img', 'src', src(0));
+    browser.assert.attribute('#image-container-top-rated > img', 'src', src(0), 'Make sure your image is wrapped in a tag with the id "image-container-top-rated"');
     data.discography.topRated.forEach(function(album, index) {
       browser.click(`.top-rated:nth-of-type(${index + 1})`);
-      browser.assert.attribute('#image-container-top-rated > img', 'src', album.art);
+      browser.assert.attribute('#image-container-top-rated > img', 'src', album.art, 'Make sure your image is wrapped in a tag with the id "image-container-top-rated"');
     })
     done();
   });
 
 	it('should have ten recording titles', function(done){
-		browser.assert.elements('.recording-title', 10);
+		browser.assert.elements('.recording-title', 10, 'Make sure your list items have the class "recording-title"');
 		done();
 	});
 
+  it('should change recordings album image when clicking an album name', function(done){
+    const src = num => data.discography.recordings[num].art;
+    browser.assert.attribute('#image-container-recording > img', 'src', src(0));
+    data.discography.recordings.forEach(function(album, index) {
+      browser.click(`.recording:nth-of-type(${index + 1})`);
+      browser.assert.attribute('#image-container-recording > img', 'src', album.art, 'Make sure your image is wrapped in a tag with the id "image-container-recording"');
+    })
+    done();
+  });
+
+  it('should have a table for all rider data', function(done){
+    browser.assert.elements('table', 1);
+    browser.assert.elements('tr', 14);
+    browser.assert.elements('td', 28);
+    done();
+  });
 });
