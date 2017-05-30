5a92a6a53f45e7a27eb2acc325c237d3654c93f6
Olivia Sabo-Rush <liv@Olivias-MacBook.local>
2017-05-30 13:57:16 -0500

    Update tests to make them less specific

--- test/index.spec.js
+++ test/index.spec.js
@@ -23,7 +23,7 @@ describe('Billypedia', function(){
   });
 
   it('should have three images on page', function(done){
-    browser.assert.elements('img', 3);
+    browser.assert.elements('img', {atLeast: 3}, "Make sure you have an IMAGE for Billy, top-rated albums, and recording albums");
     done();
   });
 
@@ -37,37 +37,47 @@ describe('Billypedia', function(){
   });
 
 	it('should have five top rated titles', function(done){
-		browser.assert.elements('.top-rated-title', 5, 'Make sure your list items have the class "top-rated-title"');
+		browser.assert.elements('#list-top-rated > *', 5, 'Make sure you make a LIST ITEM for each of the top-rated albums, and append them to the "#list-top-rated" UL');
 		done();
 	});
 
   it('should change top rated album image when clicking an album name', function(done){
     const src = num => data.discography.topRated[num].art;
-    browser.assert.attribute('#image-container-top-rated > img', 'src', src(0), 'Make sure your image is wrapped in a tag with the id "image-container-top-rated"');
+    browser.assert.attribute('#section-top-rated > img', 'src', src(0), 'Make sure your image is wrapped in a tag with the id "section-top-rated"');
     data.discography.topRated.forEach(function(album, index) {
-      browser.click(`.top-rated:nth-of-type(${index + 1})`);
-      browser.assert.attribute('#image-container-top-rated > img', 'src', album.art, 'Make sure your image is wrapped in a tag with the id "image-container-top-rated"');
+      browser.click(`#section-top-rated > ul > *:nth-of-type(${index + 1})`);
+      browser.assert.attribute('#section-top-rated > img', 'src', album.art, 'Make sure your image is wrapped in a tag with the id "section-top-rated"');
     })
     done();
   });
 
+	it('should have a section with the id "section-recordings"', function(done){
+		browser.assert.elements('section#section-recordings', 1, 'Make sure you have a SECTION with the ID "section-recordings"');
+		done();
+	});
+
+	it('should have a ul with the id "list-recordings"', function(done){
+		browser.assert.elements('#section-recordings > ul#list-recordings', 1, 'Make sure you have a UL with the ID "list-recordings" as a child of the "section-recordings" SECTION');
+		done();
+	});
+
 	it('should have ten recording titles', function(done){
-		browser.assert.elements('.recording-title', 10, 'Make sure your list items have the class "recording-title"');
+		browser.assert.elements('#list-recordings > *', 10, 'Make sure you have ten LIST ITEMS with the CLASS "recording"');
 		done();
 	});
 
   it('should change recordings album image when clicking an album name', function(done){
     const src = num => data.discography.recordings[num].art;
-    browser.assert.attribute('#image-container-recording > img', 'src', src(0));
+    browser.assert.attribute('#section-recording > img', 'src', src(0));
     data.discography.recordings.forEach(function(album, index) {
-      browser.click(`.recording:nth-of-type(${index + 1})`);
-      browser.assert.attribute('#image-container-recording > img', 'src', album.art, 'Make sure your image is wrapped in a tag with the id "image-container-recording"');
+      browser.click(`#section-recordings > ul > *:nth-of-type(${index + 1})`);
+      browser.assert.attribute('#section-recordings > img', 'src', album.art, 'Make sure your IMAGE is the child of a tag with the ID "section-recordings"');
     })
     done();
   });
 
   it('should have a table for all rider data', function(done){
-    browser.assert.elements('table', 1);
+    browser.assert.elements('table', 1, "Make sure you have a TABLE");
     browser.assert.elements('tr', 14);
     browser.assert.elements('td', 28);
     done();
